f58de6eaec02e6427cd6161fc05aafcda0202e66
RolandCoku <roland.coku@fti.edu.al>
2025-01-04 17:49:41 +0100

    Initial commit: Set up the springboot project

---
+++ src/test/java/org/springboot/caloriesapp/CaloriesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springboot.caloriesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloriesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
