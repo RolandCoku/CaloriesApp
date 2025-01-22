0feb4652e1e91120a2397beb3e11534901a02dff
ikedalaze <Ikeda.Laze@fti.edu.al>
2025-01-22 19:04:52 +0100

    Added FoodEntryAdminDTO class to separate data transfer between admin and user

--- src/main/java/org/springboot/caloriesapp/dto/FoodEntryDTO.java
+++ src/main/java/org/springboot/caloriesapp/dto/FoodEntryDTO.java
@@ -7,19 +7,17 @@ public class FoodEntryDTO {
     private String name;
     private Double calories;
     private Double price;
-    private Long userId;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
     public FoodEntryDTO() {
     }
 
-    public FoodEntryDTO(Long id, String name, Double calories, Double price, Long userId) {
+    public FoodEntryDTO(Long id, String name, Double calories, Double price) {
         this.id = id;
         this.name = name;
         this.calories = calories;
         this.price = price;
-        this.userId = userId;
     }
 
     public Long getId() {
@@ -52,14 +50,6 @@ public class FoodEntryDTO {
 
     public void setPrice(Double price) {
         this.price = price;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
     }
 
     public LocalDateTime getCreatedAt() {
